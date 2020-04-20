79c9d082bf08bc000b780ab33df74f1e6e2dd0bc
Souren Papazian <souren@papazian.info>
2020-04-20 00:42:37 -0400

    Readd sun and planet faces

--- Assets/PlanetFace.cs
+++ Assets/PlanetFace.cs
@@ -13,6 +13,7 @@ public class PlanetFace : MonoBehaviour
 
 
     public Vector3 spriteScale = new Vector3(0.23f, 0.23f, 0.1f);
+    public Vector3 spritePosition = new Vector3(0, 0, -2);
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +22,7 @@ public class PlanetFace : MonoBehaviour
         face = Instantiate(facePrefab, planetTransform);
         faceRenderer = face.GetComponent<SpriteRenderer>();
         faceTransform = face.GetComponent<Transform>();
-        faceTransform.localPosition = new Vector3(0,0, -1);
+        faceTransform.localPosition = spritePosition;
         faceTransform.localScale = spriteScale;
     }
 
